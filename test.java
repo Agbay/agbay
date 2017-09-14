a4068b017e2c3560b397051a6d56b5b2bb8aa5be
Agbay <31952991+Agbay@users.noreply.github.com>
2017-09-14 14:48:12 +0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+  public static void main(String[] args) {
+    String x  = "10";
+    add(x);
+    System.out.println(x);
+  }
+  static void add(String x){
+    x = x + 10;
+    System.out.println(x);
+  }
+}
